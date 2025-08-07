11433dab52c273835bbfefcee239229422714aa0
Dmitrii Makarov <fugas2516@gmail.com>
2025-08-07 08:40:08 -0300

    Delete js directory

--- js/form-submit.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('request-form').addEventListener('submit', async e => {
-  e.preventDefault();
-  alert('Форма отправлена (эмуляция). Интеграция с GitHub API требует серверной обработки.');
-});
