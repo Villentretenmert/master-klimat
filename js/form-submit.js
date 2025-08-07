e6b93358fb03ea1146bdc7ebb4eee31dc8be5a86
Dmitrii Makarov <fugas2516@gmail.com>
2025-08-07 08:12:02 -0300

    Add files via upload

---
+++ js/form-submit.js
@@ -0,0 +1,4 @@
+document.getElementById('request-form').addEventListener('submit', async e => {
+  e.preventDefault();
+  alert('Форма отправлена (эмуляция). Интеграция с GitHub API требует серверной обработки.');
+});
